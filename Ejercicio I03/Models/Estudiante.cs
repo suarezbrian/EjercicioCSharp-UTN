699aa7fb97e5658f036d6e8238c8c956085d340a
suarezbrian <suarezbrianalan@gmail.com>
2023-04-04 19:09:22 -0300

    add ejercicio i03

---
+++ Ejercicio I03/Models/Estudiante.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_I03.Models
+{
+    internal class Estudiante
+    {
+        private string apellido;
+        private string legajo;
+        private string nombre;
+        private int notaPrimerParcial;
+        private int notaSegundoParcial;
+        private static Random rand;
+
+        static Estudiante()
+        {
+            rand = new Random();
+        }
+
+        public Estudiante(string nombre, string apellido, string legajo)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.legajo = legajo;
+        }
+
+        public void GuardarPrimerParcial(int notaPrimerParcial)
+        {
+            this.notaPrimerParcial = notaPrimerParcial;
+        }
+
+        public void GuardarSegundoParcial(int notaSegundoParcial)
+        {
+            this.notaSegundoParcial = notaSegundoParcial;
+        }
+
+        private float CalcularPromedio()
+        {
+            return (float)((this.notaPrimerParcial + this.notaSegundoParcial) / 2);
+        }
+
+        private int NotaFinal()
+        {
+            int notaFinal = -1;
+            if (this.CalcularPromedio() > 4)
+            {
+                notaFinal  = Estudiante.rand.Next(6, 10);
+            }
+            return notaFinal;
+        }
+
+        public StringBuilder MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Nombre {this.nombre}, Apellido {this.apellido}, Legajo {this.legajo}");
+            sb.AppendLine($"Nota primer parcial : {this.notaPrimerParcial}, nota segundo parcial : {this.notaSegundoParcial}");
+            sb.AppendLine($"Promedio : {this.CalcularPromedio()}");
+
+            if (this.NotaFinal() > 0)
+            {
+                sb.AppendLine($"Nota final : {this.NotaFinal()}");
+            }
+            else
+            {
+                sb.AppendLine($"Alumno desaprobado");
+            }
+
+            return sb;
+        }
+    }
+}
--- Ejercicio I03/Program.cs
+++ Ejercicio I03/Program.cs
@@ -1,7 +1,27 @@
-﻿internal class Program
+﻿using Ejercicio_I03.Models;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Estudiante estudianteUno = new Estudiante("Jose", "Pepito", "0001");
+        estudianteUno.GuardarPrimerParcial(6);
+        estudianteUno.GuardarSegundoParcial(6);
+        Console.WriteLine(estudianteUno.MostrarDatos());
+
+
+        Estudiante estudianteDos = new Estudiante("Juan", "Garcia", "0002");
+        estudianteDos.GuardarPrimerParcial(8);
+        estudianteDos.GuardarSegundoParcial(9);
+        Console.WriteLine(estudianteDos.MostrarDatos());
+
+
+        Estudiante estudianteTres = new Estudiante("Perez", "Sanchez", "0002");
+        estudianteTres.GuardarPrimerParcial(3);
+        estudianteTres.GuardarSegundoParcial(2);
+        Console.WriteLine(estudianteTres.MostrarDatos());
+
+        Console.ReadKey();
+
     }
 }
