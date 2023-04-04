726dd7d40a9a80f9dd074b66ede89fe65b7d384f
suarezbrian <suarezbrianalan@gmail.com>
2023-04-04 12:24:33 -0300

    add ejercicios

---
+++ Ejercicio I02/Program.cs
@@ -0,0 +1,19 @@
+﻿using Entidades;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    { 
+        PersonaI02 obj1 = new PersonaI02("Brian", new DateTime(1997, 12, 17), 40123432);
+        obj1.MostrarDatos();
+        obj1.MostrarSiEsMayor();
+        PersonaI02 obj2 = new PersonaI02("Lucas", new DateTime(1987, 01, 27), 20153462);
+        obj2.MostrarDatos();
+        obj2.MostrarSiEsMayor();
+        PersonaI02 obj3 = new PersonaI02("Dario", new DateTime(2017, 03, 07), 30127832);
+        obj3.MostrarDatos();
+        obj3.MostrarSiEsMayor();
+        Console.ReadKey();
+    }
+}
---
+++ Entidades/PersonaI02.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class PersonaI02
+    {
+        private string nombre;
+        private DateTime fechaDeNacimiento;
+        private int dni;
+
+        public PersonaI02(string nombre, DateTime fechaDeNacimiento, int dni)
+        {
+            this.nombre = nombre;
+            this.fechaDeNacimiento = fechaDeNacimiento;
+            this.dni = dni;
+        }
+
+        public void GuardarNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public string ObtenerNombre()
+        {
+            return this.nombre;
+        }
+
+        public void GuardarFechaNacimiento(DateTime fechaDeNacimiento)
+        {
+            this.fechaDeNacimiento = fechaDeNacimiento;
+        }
+
+        public DateTime ObtenerFechaNacimiento()
+        {
+            return this.fechaDeNacimiento;
+        }
+
+        private int CalcularEdad()
+        {
+            return DateTime.Now.Year - this.fechaDeNacimiento.Year;
+        }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine($"Nombre : {this.nombre}, Fecha de Nacimiento : {this.fechaDeNacimiento}, DNI: {this.dni}, Edad : {this.CalcularEdad()}");
+        }
+
+        public void MostrarSiEsMayor()
+        {
+            if(this.EsMayor())
+            {
+                Console.WriteLine($"{this.nombre} es mayor de edad.");
+            }
+            else
+            {
+                Console.WriteLine($"{this.nombre} es menor de edad.");
+            }
+        }
+
+        public bool EsMayor()
+        {
+            if(this.CalcularEdad() > 18)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
