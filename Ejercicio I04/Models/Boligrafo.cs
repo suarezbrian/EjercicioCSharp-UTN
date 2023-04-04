f99e6860ce388bf059c7e8f85d7c190436fafc49
suarezbrian <suarezbrianalan@gmail.com>
2023-04-04 19:51:42 -0300

    add ejercicio i04

---
+++ Ejercicio I04/Models/Boligrafo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_I04.Models
+{
+    internal class Boligrafo
+    {
+        private const short cantidadTintaMaxima = 100;
+        private ConsoleColor color;
+        private short tinta;
+
+        public Boligrafo(ConsoleColor color, short tinta)
+        {
+            this.color = color;
+            this.tinta = tinta;
+        }
+
+        public ConsoleColor ObtenerColor()
+        {
+            return this.color;
+        }
+
+        public short ObtenerTinta()
+        {
+            return this.tinta;                 
+        }
+
+        private void GuardarTinta(short tinta)
+        {
+            if((this.tinta + tinta) >= 0 && (this.tinta + tinta) <= cantidadTintaMaxima)
+            {
+                this.tinta = tinta;
+            }
+        }
+
+        public void RecargarTinta()
+        {
+            GuardarTinta(cantidadTintaMaxima);
+        }
+
+        public bool Pintar(short gasto, out string dibujo)
+        {
+            bool esValido = false;
+            if(gasto <= this.tinta)
+            {
+                esValido = true;
+                this.tinta -= gasto;
+                dibujo = $"* - gasto un total de {gasto} de tinta";
+                return esValido;
+            }
+
+            dibujo = "Tinta Vacia";
+            return esValido;
+        }
+    }
+}
---
+++ Ejercicio I04/Program.cs
@@ -0,0 +1,46 @@
+﻿using Ejercicio_I04.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Boligrafo azul = new Boligrafo(ConsoleColor.Blue, 100);
+        Boligrafo rojo = new Boligrafo(ConsoleColor.Red, 50);
+        string dibujo;
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"Bolígrafo azul: color {azul.ObtenerColor()}, tinta {azul.ObtenerTinta()}");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Bolígrafo rojo: color {rojo.ObtenerColor()}, tinta {rojo.ObtenerTinta()}");
+
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.Red;
+        rojo.Pintar(50, out dibujo);
+        Console.WriteLine(dibujo);
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        azul.Pintar(10, out dibujo);
+        Console.WriteLine(dibujo);
+
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"Bolígrafo rojo: color {azul.ObtenerColor()}, tinta {azul.ObtenerTinta()}");
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Bolígrafo rojo: color {rojo.ObtenerColor()}, tinta {rojo.ObtenerTinta()}");
+
+        Console.WriteLine();
+        rojo.Pintar(50, out dibujo);
+        Console.WriteLine(dibujo);
+
+        rojo.RecargarTinta();
+        Console.WriteLine("Tinta Cargada al Maxinmo");
+
+        Console.WriteLine();
+        rojo.Pintar(50, out dibujo);
+        Console.WriteLine(dibujo);
+        Console.WriteLine($"Bolígrafo rojo: color {rojo.ObtenerColor()}, tinta {rojo.ObtenerTinta()}");
+
+        Console.ReadKey();
+    }
+}
