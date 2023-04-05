afac474927f2cd265e7d537f19b5708b7d011556
suarezbrian <suarezbrianalan@gmail.com>
2023-04-05 10:19:24 -0300

    add

--- EjercicioSobreCarga/Program.cs
+++ EjercicioSobreCarga/Program.cs
@@ -5,12 +5,18 @@ internal class Program
     private static void Main(string[] args)
     {
         Cosa cosa = new Cosa();
-
-        //cosa.EstablecerValor(10);
-        //cosa.EstablecerValor("Texto");
-        //cosa.EstablecerValor(DateTime.Now);
-
         Console.WriteLine(Cosa.Mostrar(cosa));
+
+        cosa.EstablecerValor(102);
+        cosa.EstablecerValor("Desde el metodo");
+        cosa.EstablecerValor(DateTime.Now);
+        Console.WriteLine(Cosa.Mostrar(cosa));
+
+        cosa = new Cosa("SobreCarga");
+        cosa = new Cosa(100,"SobreCarga");
+        cosa = new Cosa(DateTime.Now, 101, "SobreCarga");
+        Console.WriteLine(Cosa.Mostrar(cosa));
+
 
         Console.ReadKey();
     }
